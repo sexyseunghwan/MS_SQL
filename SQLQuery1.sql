3ea130f11aa9046a0a99873520eb10c02a1d292b
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-11-04 18:30:15 +0900

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,11 +1,6 @@
-
-
-
---ms sql 연습문제
-
 go
 
--- 1. Practice > SQL > Alternative Queries > Print Prime Numbers
+-- 1. Practice > SQL > Alternative Queries > Print Prime Numbers(***)
 -- https://www.hackerrank.com/challenges/print-prime-numbers/problem
 
 declare @num int, @index int, @answer varchar(1500)
@@ -37,3 +32,43 @@ from employees
 
 
 go
+
+
+--3. Practice > SQL > Aggregation > Top Earners(***)
+-- https://www.hackerrank.com/challenges/earnings-of-employees/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+select 
+    max(salary*months), count(*) 
+from employee 
+where salary*months = (select max(salary*months) from employee)
+
+
+--4. Practice > SQL > Alternative Queries > Draw The Triangle 2(***)
+-- https://www.hackerrank.com/challenges/draw-the-triangle-2/problem
+
+declare @number int
+set @number = 1
+while @number <= 20
+begin
+    print replicate('* ',@number)
+    set @number = @number + 1
+end
+
+--5. Practice > SQL > Aggregation > Weather Observation Station 2
+-- https://www.hackerrank.com/challenges/weather-observation-station-2/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
