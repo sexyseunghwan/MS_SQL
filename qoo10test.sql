11acd8e746ac1fcfbbc0a9994dcdd0cbd72a6a76
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-03-23 23:16:00 +0900

    2021-03-23

--- qoo10test.sql
+++ qoo10test.sql
@@ -24,6 +24,45 @@ having count(*) > 1
 
 drop table dbo.QOO10USER
 
+
+
+
+--drop proc dbo.qoo10_dummy_init
+
+/*
+	Author      : Seunghwan Shin
+	Create date : 2021-02-20 
+	Description : 더미데이터 생성 : 회원관련
+	    
+	History		: 2021-02-20 Seunghwan Shin	#최초 생성
+
+*/create proc [dbo].[qoo10_dummy_init]	@id varchar(100),	@pw varchar(100),	@email varchar(200),	@gender char(1),	@nation char(2),	@ipaddress varchar(200),	@coin intasset nocount on
+set transaction isolation level read uncommittedbegin	insert into dbo.QOO10USER values (@id,@pw,@email,@gender,@nation,@ipaddress,@coin)	if @@error <> 0	begin		return 0;	endend
+
+
+--drop table dbo.QOO10USERENC
+
+-- QOO10 의 회원 테이블(더미데이터로 만든것) : 암호화작업 확인
+create table dbo.QOO10USERENC 
+( 
+	usercode int identity(1,1) not null,--회원고유코드 
+	id varchar(100) not null,--회원 아이디
+	pw varchar(100) not null,--회원 비밀번호
+	email varchar(200) null,--회원 이메일 
+	gender char(1) null,--회원 성별 
+	nation char(2) null,-- 회원 국가 
+	ipaddress varchar(200) null,-- 회원 아이피주소
+	hascoin int ,-- 회원이 소유한 코인
+	pw_encryption varchar(800) not null -- 회원 비밀번호 encryption
+)
+
+ALTER TABLE dbo.QOO10USERENC ADD CONSTRAINT PK__QOO10USERENC__USERCODE PRIMARY KEY CLUSTERED (usercode)
+
+
+
+
+
+
 -- QOO10 의 회원 테이블(더미데이터로 만든것)
 create table dbo.QOO10USER 
 ( 
@@ -757,3 +796,5 @@ begin
 
 
 end
+
+
--- 실행계획.sql
+++ 실행계획.sql
@@ -27,12 +27,12 @@ set STATISTICS TIME on
 				-- dbo.APPLEBUYTBL ab
 				--dbo.APPLEINC ac
 
-				select qu.id,qu.pw,qu.email from dbo.APPLEINC ac with(nolock) 
-				inner join dbo.APPLEBUYTBL ab with(nolock) on ac.prodserial = ab.prodserial 
-				left join dbo.QOO10USER qu with(nolock) on qu.usercode = ab.userseq 
-				where qu.nation = 'KR'
-				and qu.gender = 'F'  
-				and ac.prodname = 'ipad mini 3th'
+				--select qu.id,qu.pw,qu.email from dbo.APPLEINC ac with(nolock) 
+				--inner join dbo.APPLEBUYTBL ab with(nolock) on ac.prodserial = ab.prodserial 
+				--left join dbo.QOO10USER qu with(nolock) on qu.usercode = ab.userseq 
+				--where qu.nation = 'KR'
+				--and qu.gender = 'F'  
+				--and ac.prodname = 'ipad mini 3th'
 
 
 
